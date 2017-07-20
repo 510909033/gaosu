ed87e5d5994321da239d8de046fbc686c0e6f1bb
邸万超 <29862285@qq.com>
2017-07-20 11:48:19 +0800

    111

--- vendor/WxpayAPI/lib/WxPay.Config.php
+++ vendor/WxpayAPI/lib/WxPay.Config.php
@@ -22,8 +22,8 @@ class WxPayConfig
 	 * 获取地址：https://mp.weixin.qq.com/advanced/advanced?action=dev&t=advanced/dev&token=2005451881&lang=zh_CN
 	 * @var string
 	 */
-	const APPID = 'wx426b3015555a46be';
-	const MCHID = '1900009851';
+	const APPID = 'wx43cccee49cb479ff';
+	const MCHID = '1482887882';
 	const KEY = '8934e7d15453e97507ef794cf7b0519d';
 	const APPSECRET = '7813490da6f1265e4901ffb80afaa36f';
 	
