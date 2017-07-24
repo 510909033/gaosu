5eab0287dd6b4115a00d9a5678179a931eeaf45a
510909033 <Administrator@510909033@qq.com>
2017-07-24 13:43:24 +0800

    调制微信授权

--- application/way/controller/UserController.php
+++ application/way/controller/UserController.php
@@ -7,6 +7,8 @@ use think\Request;
 use app\way\controller\func\UserBindCarFuncController;
 use app\common\model\WayUserBindCar;
 use think\Validate;
+use weixin\auth\AuthController;
+use app\common\model\SysConfig;
 
 class UserController extends Controller
 {
@@ -25,6 +27,32 @@ class UserController extends Controller
         
         
         dump($res);
+        
+    }
+    
+    public function initAconfigAction(){
+        $config = new SysConfig();
+        $config->init_table_data();
+        
+    }
+    
+    public function auth(){
+        
+        $auth = new AuthController();
+        $redirect_uri = url('way/user/return_url');
+        $state = 'web';
+        $is_unit = false;
+        $auth->redirect($redirect_uri, $state, $is_unit);
+        
+    }
+    
+    public function return_url(){
+        
+        $auth = new AuthController();
+        $user = $auth->getResultByCode();
+        
+        dump($user);
+        
         
     }
     
