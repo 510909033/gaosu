b33664a201570b377a23f497d0845c22f2667606
gaosu <420360862@qqcom>
2017-07-26 08:56:55 +0800

    11

--- application/test/controller/IndexController.php
+++ application/test/controller/IndexController.php
@@ -33,7 +33,40 @@ class IndexController
 
         	exit;
         }
-       
+
     }
+
+//获取access_token
+    public function access(){
+        
+        //定义一个APPID的常量
+        define('APP_ID','wx9e1d8fc5ee0c85a1');
+
+        //定义一个APPSECRET的常量
+        define('APP_SECRET','39ea8dc418b5ab3a03867a5937fe19fd'); 
+
+        //使用CURL向腾讯的API接口中发送对应的请求
+        $ch = curl_init();
+
+        $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.APP_ID.'&secret='.APP_SECRET;
+
+        curl_setopt($ch,CURLOPT_URL,$url);
+
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+
+        curl_setopt($ch,CURLOPT_HEADER,1);
+
+        curl_setopt($ch,CURLOPT_TIMEOUT,10);
+
+        $output = curl_exec($ch);
+
+        var_dump($output);
+
+        
+        //得到access_token（票据）
+        //
+        //将其保存起来
+    }
+       
 
 }
