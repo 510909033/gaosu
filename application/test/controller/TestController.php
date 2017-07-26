1c52698440542523f61f7937aa0197570b83e27f
gaosu <420360862@qqcom>
2017-07-26 16:01:00 +0800

    11

--- application/test/controller/TestController.php
+++ application/test/controller/TestController.php
@@ -4,8 +4,10 @@ namespace app\test\controller;
 class TestController
 {
     //
+   public function aa(){
     $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
 
     file_put_contents('demo.txt','$postStr');
+	}
   
 }
