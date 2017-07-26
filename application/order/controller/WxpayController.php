69873e744e7883f4f9d9c08c0e50a0c2de5a69cf
邸万超 <510909033>
2017-07-26 14:39:19 +0800

    111

--- application/order/controller/WxpayController.php
+++ application/order/controller/WxpayController.php
@@ -29,7 +29,7 @@ class WxpayController extends Controller
 /*         $orderid = $_GET['id'];
         if (!isset($orderid) || empty($orderid) || !is_numeric($orderid))
             $this->error('查询不到正确的订单信息'); */
-        $orderid = '201707191700278553924515';
+        $orderid = '201707191700278553924516';
             
         $orderdate = SysOrder::get(['out_trade_no'=>$orderid]);
        
