1fcede28fb01494359c6194b387d0e5cc41d7bdc
邸万超 <510909033>
2017-07-27 13:28:45 +0800

    11

--- vendor/WxpayAPI/example/notify.php
+++ vendor/WxpayAPI/example/notify.php
@@ -38,17 +38,21 @@ class PayNotifyCallBack extends WxPayNotify
 		$update = array();
 		if(!array_key_exists("transaction_id", $data)){
 			$msg = "输入参数不正确";
+			Log::write($msg);
 			return false;
 		}
 		//查询订单，判断订单真实性
 		if(!$this->Queryorder($data["transaction_id"])){
 			$msg = "订单查询失败";
+			Log::write($msg.$data['out_trade_no']);
 			return false;
 		}
         $orderdata =  SysOrder::get(['out_trade_no'=>$data['out_trade_no']]);
-        if (!$orderdata)
+        if (!$orderdata){
             $msg = '本地订单不存在';
+            Log::write($msg.$data['out_trade_no']);
             return false;
+        }
         if ($orderdata['total_fee']!=$data['total_fee']){
             $update['remark'] = '金额异常';
             $update['trade_state'] = 'PAYERROR';
