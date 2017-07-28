48a0456e6260b6640a5c8aea9612e5a3cbc22f19
gaosu <420360862@qqcom>
2017-07-28 08:45:16 +0800

    11

--- application/test/controller/IndexController.php
+++ application/test/controller/IndexController.php
@@ -3,12 +3,19 @@ namespace app\test\controller;
 
 use think\Controller;
 class IndexController extends Controller {
+    protected function _initialize()
+    {
+        $this->log('__construct');
+    }
 
-	public function __construct(){
-		
+	private function log($str){
+	    $filename = time().'.txt';
+	    @file_put_content('./'.$filename , $str .PHP_EOL.PHP_EOL,FILE_APPEND);
 	}
-
 	public function indexAction(){
+	    $this->log(11);
+	    $this->log($GLOBALS['HTTP_RAW_POST_DATA']);
+	    
 		//获得参数 signature nonce token timestamp echostr
 		$nonce     = $_GET['nonce'];
 		$token     = 'zhgs';
@@ -22,10 +29,12 @@ class IndexController extends Controller {
 		//拼接成字符串,sha1加密 ，然后与signature进行校验
 		$str = sha1( implode( $array ) );
 		if( $str  == $signature && $echostr ){
+		    $this->log(22);
 			//第一次接入weixin api接口的时候
 			echo  $echostr;
 			exit;
 		}else{
+		    $this->log(33);
 			$this->reponseMsgAction();
 		}
 	}
@@ -51,8 +60,10 @@ class IndexController extends Controller {
 		// gh_e79a177814ed
 		//判断该数据包是否是订阅的事件推送
 		if( strtolower( $postObj->MsgType) == 'event'){
+		    $this->log(44);
 			//如果是关注 subscribe 事件
 			if( strtolower($postObj->Event == 'subscribe') ){
+			    $this->log(55);
 				//回复用户消息(纯文本格式)	
 				$toUser   = $postObj->FromUserName;
 				$fromUser = $postObj->ToUserName;
@@ -68,6 +79,9 @@ class IndexController extends Controller {
 							</xml>";
 				$info     = sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
 				echo $info;
+				$this->log($info);
+				//
+				exit();
 /*<xml>
 <ToUserName><![CDATA[toUser]]></ToUserName>
 <FromUserName><![CDATA[fromUser]]></FromUserName>
