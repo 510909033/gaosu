87900cc3c8dfe99808f696135317c6a5804525eb
gaosu <420360862@qqcom>
2017-07-28 13:39:23 +0800

    11

--- application/test/controller/IndexController.php
+++ application/test/controller/IndexController.php
@@ -8,17 +8,16 @@ class IndexController extends Controller {
         $this->log('__construct');
     }
 
-	private function log($str){
+	/*private function log($str){
 	    $filename = time().'.txt';
 	    @file_put_contents('./'.$filename , $str .PHP_EOL.PHP_EOL,FILE_APPEND);
-	}
+	}*/
 	public function indexAction(){
-	    $this->log(11);
-	    //$this->log($GLOBALS['HTTP_RAW_POST_DATA']);
+	    //$this->log(11);
+	    $this->log($GLOBALS['HTTP_RAW_POST_DATA']);
 	    
 		//获得参数 signature nonce token timestamp echostr
 		$nonce     = $_GET['nonce'];
-		var_dump($nonce);
 		$token     = 'zhgs';
 		$timestamp = $_GET['timestamp'];
 		$echostr   = $_GET['echostr'];
@@ -30,12 +29,12 @@ class IndexController extends Controller {
 		//拼接成字符串,sha1加密 ，然后与signature进行校验
 		$str = sha1( implode( $array ) );
 		if( $str  == $signature && $echostr ){
-		    $this->log(22);
+		    //$this->log(22);
 			//第一次接入weixin api接口的时候
 			echo  $echostr;
 			exit;
 		}else{
-		    $this->log(33);
+		    //$this->log(33);
 			$this->reponseMsgAction();
 		}
 	}
@@ -64,10 +63,10 @@ class IndexController extends Controller {
 		// gh_e79a177814ed
 		//判断该数据包是否是订阅的事件推送
 		if( strtolower( $postObj->MsgType) == 'event'){
-		    $this->log(44);
+		   // $this->log(44);
 			//如果是关注 subscribe 事件
 			if( strtolower($postObj->Event == 'subscribe') ){
-			    $this->log(55);
+			   // $this->log(55);
 				//回复用户消息(纯文本格式)	
 				$toUser   = $postObj->FromUserName;
 				$fromUser = $postObj->ToUserName;
