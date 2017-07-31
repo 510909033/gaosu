65164584df91d615fa0fcbe1fed244f6d7c566a5
gaosu <420360862@qqcom>
2017-07-31 17:07:13 +0800

    1

--- application/test/controller/IndexController.php
+++ application/test/controller/IndexController.php
@@ -60,7 +60,7 @@ class IndexController extends Controller {
 		}
 
 		//用户发送tuwen1关键字的时候，回复一个单图文
-		if( strtolower($postObj->MsgType) == 'text' && trim($postObj->Content)=='tuwen2' ){
+		if( strtolower($postObj->MsgType) == 'text' && trim($postObj->Content)=='腾放' ){
 			$toUser = $postObj->FromUserName;
 			$fromUser = $postObj->ToUserName;
 			$arr = array(
