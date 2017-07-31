8f0e2b7a3f16ca9b565096be98cb06b3a1b5b11f
gaosu <420360862@qqcom>
2017-07-31 11:05:34 +0800

    1

--- application/test/controller/IndexController.php
+++ application/test/controller/IndexController.php
@@ -132,7 +132,7 @@ class IndexController extends Controller {
 		//创建微信菜单
 		//目前微信接口的调用方式都是通过curl post/get
 		header('content-type:text/html;charset=utf-8')
-		 $access_token = $this->getWxAccessTokenAction();
+		 echo $access_token = $this->getWxAccessTokenAction();
 		 $url = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=".$access_token;
 		 $postArr = array(
 		 			'button'=>array(
