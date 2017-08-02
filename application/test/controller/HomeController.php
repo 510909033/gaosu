c7b878ee7dfb3bce073d73129840102bb8261775
gaosu <420360862@qqcom>
2017-08-02 08:53:03 +0800

    11

---
+++ application/test/controller/HomeController.php
@@ -0,0 +1,3 @@
+<?php
+$json=file_get_contents("http://wthrcdn.etouch.cn/weather_mini?city=长春");
+var_dump($josn);
--- application/test/controller/IndexController.php
+++ application/test/controller/IndexController.php
@@ -171,7 +171,8 @@ class IndexController extends Controller {
 				if(empty($data->data)){
 					$contentStr = "抱歉，没有查到\"".$str_key."\"的天气信息！";
 				} else {
-					$contentStr = "【".$data->data->city."天气预报】\n".$data->data->date_y." ".$data->data->fchh."时发布"."\n\n实时天气\n";//.$data->weatherinfo->weather1." ".$data->weatherinfo->temp1." ".$data->weatherinfo->wind1."\n\n温馨提示：".$data->weatherinfo->index_d."\n\n明天\n".$data->weatherinfo->weather2." ".$data->weatherinfo->temp2." ".$data->weatherinfo->wind2."\n\n后天\n".$data->weatherinfo->weather3." ".$data->weatherinfo->temp3." ".$data->weatherinfo->wind3;
+					$contentStr = "【".$data->data->city."天气预报】\n".$data->data->aqi." ".$data->data->fx."时发布"."\n\n实时天气\n";//.$data->weatherinfo->weather1." ".$data->weatherinfo->temp1." ".$data->weatherinfo->wind1."\n\n温馨提示：".$data->weatherinfo->index_d."\n\n明天\n".$data->weatherinfo->weather2." ".$data->weatherinfo->temp2." ".$data->weatherinfo->wind2."\n\n后天\n".$data->weatherinfo->weather3." ".$data->weatherinfo->temp3." ".$data->weatherinfo->wind3;
+					var_dump($contentStr);
 				}
 			}
 			$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
@@ -215,7 +216,7 @@ class IndexController extends Controller {
 		include("weather_cityId.php");
 		$c_name=$weather_cityId[$n];
 		if(!empty($c_name)){
-			$json=file_get_contents("http://wthrcdn.etouch.cn/weather_mini?city=".$c_name);
+			$json=file_get_contents("http://wthrcdn.etouch.cn/weather_mini?city=长春";)//.$c_name);
 			return json_decode($json);
 		} else {
 			return null;
