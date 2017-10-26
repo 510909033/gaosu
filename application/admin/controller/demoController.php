ad5377d70ecf0ac959de24b7872fa6fcb4f34394
diwanchao <29862285@qq.com>
2017-10-26 15:37:11 +0800

    111

--- application/admin/controller/demoController.php
+++ application/admin/controller/demoController.php
@@ -86,7 +86,14 @@ class demoController extends Controller
 
 
     public function testAction(){
-        $res= Db::query("SELECT * FROM `kw_station` WHERE Station_ID=134");
-        var_dump($res);
+        $res= Db::query("SELECT * FROM `kw_station` WHERE Station_ID!=311 or Station_ID!='31A'");
+        foreach ($res as $key => $value) {
+            $sql='UPDATE kw_station SET Station_Name="'.iconv("gb2312", "utf-8", $value['Station_Name']).'"WHERE Station_ID="'.$value['Station_ID'].'"';           
+            $res = Db::execute($sql);
+        }
+        echo "done";
+
+/*        $data = iconv("gb2312","utf-8",$res[0]['Station_Name']);
+        var_dump($data);*/
     }
 }
